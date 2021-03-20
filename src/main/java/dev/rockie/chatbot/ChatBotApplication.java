7de1ed1769f4173d95de68dd805ff029eed6ec19
rockie-xu <klenovyj7list@gmail.com>
2021-03-20 09:34:53 +0100

    bot set to the basic state

--- src/main/java/dev/rockie/chatbot/ChatBotApplication.java
+++ src/main/java/dev/rockie/chatbot/ChatBotApplication.java
@@ -1,13 +1,21 @@
 package dev.rockie.chatbot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import dev.rockie.chatbot.controller.ChatbotController;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-@SpringBootApplication
 public class ChatBotApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ChatBotApplication.class, args);
+
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new ChatbotController());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
+
 
 }
--- src/main/java/dev/rockie/chatbot/controller/ChatbotController.java
+++ src/main/java/dev/rockie/chatbot/controller/ChatbotController.java
@@ -1,21 +1,39 @@
 package dev.rockie.chatbot.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+public class ChatbotController extends TelegramLongPollingBot {
 
-@RestController
-@RequestMapping("/")
-public class ChatbotController {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(ChatbotController.class);
-
-    @GetMapping("/hello")
-    public String helloWorld() {
-
-        return "hello World";
+    @Override
+    public String getBotUsername() {
+        return "LearnCapitals_Bot";
     }
+
+    @Override
+    public String getBotToken() {
+
+        return null;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
+            message.setChatId(update.getMessage().getChatId().toString());
+            message.setText(update.getMessage().getText());
+
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
